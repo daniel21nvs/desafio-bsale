87bfa16f976f8e52f5922b55e9fcf102243ad0c0
Daniel <daniel21nvs@gmail.com>
2021-11-16 19:45:48 -0300

    make it better

--- src/main/java/com/desafioBsale/desafioBsale/controller/CategoriaController.java
+++ src/main/java/com/desafioBsale/desafioBsale/controller/CategoriaController.java
@@ -3,12 +3,14 @@ package com.desafioBsale.desafioBsale.controller;
 import com.desafioBsale.desafioBsale.model.Category;
 import com.desafioBsale.desafioBsale.service.CategoryServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class CategoriaController {
